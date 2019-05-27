22e3ece4cfa5c6bc4cf54927d88b3c9ed9f16771
qiliang.sun <qiliang.sun@innoic.com>
2019-05-27 17:38:46 +0800

    增加多线程处理

---
+++ multiprocess.py
@@ -0,0 +1,105 @@
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+import cv2
+import sys
+import threading
+from Queue import Queue
+import time
+
+live_url = "rtsp://admin:admin123@172.16.1.29/cam/realmonitor?channel=1&subtype=0"
+record_url = "rtsp://admin:admin123@172.16.1.16:554/cam/playback?channel=1&subtype=0&starttime=2019_05_27_17_05_00&endtime=2019_05_27_17_06_00"
+
+threadLock = threading.Lock()
+queue = Queue()
+
+class GetPicture(threading.Thread):
+
+	def __init__(self, queue):
+		threading.Thread.__init__(self, name = "GetPicture")
+		self.frame_queue = queue
+		self.capture = cv2.VideoCapture(live_url)
+		self.stop_flag = False
+
+		# 打印视频相关参数，帧率，宽高
+		if self.capture.isOpened():
+			print (self.capture.get(cv2.CAP_PROP_FPS))
+			print (self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+			print (self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+		self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+		# self.eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+
+	def run(self):
+			
+		while 1:
+			# Capture frame-by-frame
+			ret, frame = self.capture.read()
+			# cv2.imshow('image_ori', frame)
+
+			self.frame_queue.put(frame)
+			print("---- put frame.")
+
+		self.capture.release()
+		cv2.destroyAllWindows()
+
+			
+	def stop(self):
+		self.stop_flag = True
+		self.capture.release()
+		
+
+class ShowPicture(threading.Thread):
+
+	def __init__(self, queue):
+		threading.Thread.__init__(self, name = "ShowPicture")
+		self.frame_queue = queue
+		self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+		# self.eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+		self.stop_flag = False
+
+
+	def run(self):
+		
+		while 1:
+			# get接口默认为阻塞接口，会一直等待数据
+			frame = self.frame_queue.get()
+			print("get frame. left %d"% (self.frame_queue.qsize()))
+			
+			# opencv读取的图片格式为bgr24 转为灰度图
+			frame_temp = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			# 直方图均匀化(改善图像的对比度和亮度)
+			frame_temp = cv2.equalizeHist(frame_temp)	
+
+			# 获取该图片中的各个人脸的坐标,画框
+			faces = self.face_cascade.detectMultiScale(frame_temp, 1.3, 5)
+			for (x, y, w, h) in faces:
+				frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+				roi_gray = frame[y:y + h, x:x + w]
+				roi_color = frame[y:y + h, x:x + w]
+
+			cv2.imshow('image',frame)
+
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+
+		cv2.destroyAllWindows()
+			
+
+	def stop(self):
+		self.stop_flag = True
+		cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+
+	producer = GetPicture(queue)
+	consumer = ShowPicture(queue)
+
+	producer.start()
+	consumer.start()
+
+	producer.join()
+	consumer.join()
+
+	print("finished!")
