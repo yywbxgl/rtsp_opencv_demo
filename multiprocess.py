7b3c54ad20e638927b2545aeff264e16fb33eaee
qiliang.sun <qiliang.sun@innoic.com>
2019-05-29 17:32:36 +0800

    修改data数据保存格式，便于解析

--- multiprocess.py
+++ multiprocess.py
@@ -6,6 +6,7 @@ import sys
 import threading, thread
 from Queue import Queue
 import time
+import json
 
 LIVE_URL = "rtsp://admin:admin123@172.16.1.29/cam/realmonitor?channel=1&subtype=0"
 RECORD_URL = "rtsp://admin:admin123@172.16.1.16:554/cam/playback?channel=1&subtype=0&starttime=2019_05_27_17_06_00&endtime=2019_05_27_17_08_00"
@@ -30,6 +31,7 @@ class DealRecord(threading.Thread):
 		self.frame_queue = Queue()
 		record_url = "rtsp://admin:admin123@172.16.1.16:554/cam/playback?channel=1&subtype=0&starttime=" + self.start_time + "&endtime=" + self.end_time
 		self.capture = cv2.VideoCapture(record_url)
+		print("get %s"%(record_url))
 
 	def run(self):
 		# 打印视频相关参数，帧率，宽高
@@ -41,6 +43,7 @@ class DealRecord(threading.Thread):
 		thread.start_new_thread(self.getRecord, ())
 
 		self.dealRecord()
+
 
 	def getRecord(self):
 		while self.capture.isOpened():
@@ -56,6 +59,7 @@ class DealRecord(threading.Thread):
 
 		print("---- get record finish.")
 		self.capture.release()
+
 
 	def dealRecord(self):
 		frame_num = 0
@@ -78,13 +82,15 @@ class DealRecord(threading.Thread):
 
 			# 获取该图片中的各个人脸的坐标,画框
 			faces = self.face_cascade.detectMultiScale(frame_temp, 1.3, 5)
-			# print(faces)
+
 			for (x, y, w, h) in faces:
 				frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-				roi_gray = frame[y:y + h, x:x + w]
-				roi_color = frame[y:y + h, x:x + w]
 
-			content = str(frame_num) + ' ' + str(faces) + "\n"
+			if '[' in str(faces):
+				content = str(faces.tolist()) + "\n"
+			else:
+				content = "[]" + '\n'
+
 			data.append(content)
 
 			# 写入文件
@@ -92,18 +98,19 @@ class DealRecord(threading.Thread):
 				self.writeFile(data)
 				data = []
 
-			# cv2.imshow('image',frame)
+			cv2.imshow('image',frame)
 
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				break
 
+		self.writeFile(data)
 		cv2.destroyAllWindows()
 		print("---- deal record finish. total frame %d"%(frame_num))
 
 
 	def writeFile(self, data):
 		start_name = time.strftime("%Y%m%d_%H%M%S", time.localtime(self.time_stamp)) 
-		file_name = "data/" + start_name + ".txt"
+		file_name = FILE_PATH + start_name + ".txt"
 		txt = open(file_name, "w")
 		for line in data:
 			txt.write(line)
@@ -118,9 +125,13 @@ class DealRecord(threading.Thread):
 
 if __name__ == "__main__":
 
-	start_time = "2019_05_27_16_15_00"
-	end_time =  "2019_05_27_16_18_00"
+	start_time = "2019_05_27_17_41_00"
+	end_time =  "2019_05_27_17_44_00"
 
+	if (len(sys.argv) == 3):
+		start_time = sys.argv[1]
+		end_time = sys.argv[2]
+	
 	test = DealRecord(start_time, end_time)
 
 	test.start()
